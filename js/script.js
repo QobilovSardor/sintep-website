8c71ae45c27ef4204560301fa080dba599cad0ac
sardor <sardorqobilov555@gmail.com>
2023-05-26 18:42:06 +0500

    spheres html finished

--- js/script.js
+++ js/script.js
@@ -1,39 +1,11 @@
-// $(function () {
-
-//     $('input[name=phone]').mask('+7 (999) 999-99-99');
-
-
-//     $('.menu-opener').on('click', function (e) {
-//         e.preventDefault();
-//         $(this).toggleClass('active');
-//         $('.navbar').toggleClass('active');
-//     });
-
-//     var swiper3 = new Swiper(".quality-slide-in", {
-//         slidesPerView: 1,
-//         spaceBetween: 10,
-//         loop: true,
-//         speed: 1000,
-//         pagination: {
-//             el: ".swiper-pagination",
-//             clickable: true,
-//         },
-//         navigation: {
-//             nextEl: ".swiper-button-next",
-//             prevEl: ".swiper-button-prev",
-//         },
-//         breakpoints: {
-//             640: {
-//                 slidesPerView: 1,
-//             },
-//             768: {
-//                 slidesPerView: 1,
-//             },
-//             1024: {
-//                 slidesPerView: 1,
-//             },
-//         },
-//     });
-
-// });
-
+$(document).ready(function () {
+	$('.accordion-content').hide(); // accordion-content elementlarni yopish
+	$('.accordion-header').click(function () {
+		$(this).next('.accordion-content').slideToggle();
+		$(this).toggleClass('active');
+		$(this).find('.icon').toggleClass('active');
+		$('.accordion-content').not($(this).next('.accordion-content')).slideUp();
+		$('.accordion-header').not($(this)).removeClass('active');
+		$('.accordion-header').not($(this)).find('.icon').removeClass('down').addClass('up');
+	});
+});
